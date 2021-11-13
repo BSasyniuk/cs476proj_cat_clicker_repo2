ad24225c4be58f195fba074c71aa9972ce8d0886
BSasyniuk <bsasyniuk@sasktel.net>
2021-11-13 15:29:55 -0600

    First Push

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 import django_heroku
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-zb79kjsi+_t+4riih6mpx0)q1kn&17qvok&11-t)z%os#)8zff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.herokuapp.com']
 
 
 # Application definitionS
@@ -76,18 +77,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'clickerusers',
+#        'USER': 'root',
+#        'PASSWORD': '21cs476fall',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#
+#   }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clickerusers',
-        'USER': 'root',
-        'PASSWORD': '21cs476fall',
-        'HOST': 'localhost',
-        'PORT': '3306',
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dclbv7glgsjemd',
+        'USER': 'gnprmshqwnrvvr',
+        'PASSWORD': 'b7b9ab83d9cba70a61282a3fa6b7770fcc0f628d2ac32f18ec8f7e4ec3f8d60d',
+        'HOST': 'ec2-44-198-236-169.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,5 +146,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
-
-django_heroku.settings(locals())
